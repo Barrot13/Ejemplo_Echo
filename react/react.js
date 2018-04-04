6ee5759baef5c76012da7c7feaf31b9797081def
Barrot13 <barrot1307@outlook.com>
2018-04-04 04:33:53 -0600

    Ejemplo
    Proyecto de ejemplo para la instalación y configuración de React

---
+++ react/react.js
@@ -0,0 +1,8 @@
+console.log('App.js Is Running');
+
+//JSX JavaScript XML
+
+var template = <h1>Ejemplo Echo</h1>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
