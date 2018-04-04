55964b619437e35ec317b3dfa8058217e50970d6
Barrot13 <barrot1307@outlook.com>
2018-04-04 17:05:45 -0600

    Ejemplo
    Material para ejemplo

--- react/react.js
+++ react/react.js
@@ -2,7 +2,59 @@ console.log('App.js Is Running');
 
 //JSX JavaScript XML
 
-var template = <h1>Ejemplo Echo</h1>;
-var appRoot = document.getElementById('app');
+/*Ejemplo 1
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
 
-ReactDOM.render(template, appRoot);
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);*/
+
+
+/*Ejemplo Reloj 1
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+*/
+
+
+
+/*Ejemplo reloj 2
+function Reloj(datos) {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {datos.hora}.</h2>
+    </div>
+  );
+  return element;
+}
+
+function tick(){
+  const reloj = <Reloj hora={new Date().toLocaleTimeString()}/>;
+  ReactDOM.render(reloj, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+*/
